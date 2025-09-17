685c50bf606ee867bfd713a8a48ce3acd2a2dc32
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2025-09-17 10:07:42 +0600

    feat(apps/subscription): update Client model

--- apps/subscription/models.py
+++ apps/subscription/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class Client(models.Model):
     name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
+    email = models.EmailField(max_length=320)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
 
 
 class Subscription(models.Model):
