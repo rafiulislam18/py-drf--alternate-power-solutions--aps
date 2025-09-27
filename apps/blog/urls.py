7db92c5314beddfd8becfe9b25b0c3c252158688
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2025-09-28 01:24:33 +0600

    vps last update

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -3,5 +3,5 @@ from .views import BlogCategoryListAPIView, BlogDetailsAPIView
 
 urlpatterns = [
     path("", BlogCategoryListAPIView.as_view(), name="blog-category-list"),
-    path('<int:blog_id>/', BlogDetailsAPIView.as_view(), name='blog-detail'),
+    path('<int:blog_id>', BlogDetailsAPIView.as_view(), name='blog-detail'),
 ]
--- apps/services_and_projects/urls.py
+++ apps/services_and_projects/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', ServiceProjectListAPIView.as_view(), name='service-project-list'),
-    path('service/<int:service_id>/', ServiceDetailsAPIView.as_view(), name='service-detail'),
-    path('project/<int:project_id>/', ProjectDetailsAPIView.as_view(), name='project-detail'),
+    path('service/<int:service_id>', ServiceDetailsAPIView.as_view(), name='service-detail'),
+    path('project/<int:project_id>', ProjectDetailsAPIView.as_view(), name='project-detail'),
     path('services/', ServiceListAPIView.as_view(), name='service-list'),
 ]
